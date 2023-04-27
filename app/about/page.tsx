df6d5a2209f1c89c6506302747fa319710eb7842
Jafar Thwahrah <jaffardawahreh2@gmail.com>
2023-04-28 02:51:31 +0300

    metadata, errors

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,12 @@
 import React from "react";
-
+import Link from "next/link";
 export default function About() {
-  return <div>About Page</div>;
+  // throw new Error("not today");
+
+  return (
+    <>
+      <h1>About Page</h1>
+      <Link href="/">Home Page</Link>
+    </>
+  );
 }
