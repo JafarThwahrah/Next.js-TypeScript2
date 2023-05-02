92cf6e64ede59fde81df30467c098d11698fc3d1
Jafar Thwahrah <jaffardawahreh2@gmail.com>
2023-05-02 15:18:39 +0300

    server Components

---
+++ next-tutorial-2/app/users/[userId]/page.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import getUser from "@/lib/getUser";
+import getUserPosts from "@/lib/getUserPosts";
+import { Suspense } from "react";
+import UserPosts from "./components/UserPosts";
+type Params = {
+  params: {
+    userId: string;
+  };
+};
+export async function generateMetadata({ params: { userId } }: Params) {
+  const userData: Promise<User> = getUser(userId);
+  const user: User = await userData;
+
+  return {
+    title: user.name,
+    descriptiosn: `This is the page of ${user.name}`,
+  };
+}
+
+export default async function UserPage({ params: { userId } }: Params) {
+  const userData: Promise<User> = getUser(userId);
+  const userPostsData: Promise<Post[]> = getUserPosts(userId);
+
+  const [user, userPosts] = await Promise.all([userData, userPostsData]);
+  return (
+    <>
+      <h2>{user.name}</h2>
+      <br />
+      <Suspense fallback={<h2>Loading...</h2>}>
+        {/* @ts-expect-error Async Server Component */}
+        <UserPosts posts={userPosts} />
+      </Suspense>
+    </>
+  );
+}
